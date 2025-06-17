55bed7ce5d95736f49514c831a8cae630059eaf5
Becca Shapiro <rshapiro3@gmail.com>
2025-06-17 12:35:54 -0600

    add json db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import db from "../db/db.json";
 
 export default function Home() {
+  console.log(db);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
